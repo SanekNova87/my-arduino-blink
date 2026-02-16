7e07e287772223573130946ba3d16480565214fe
SanekNova87 <rumyantsevaalex.2208@gmail.com>
2026-02-16 19:28:10 +0300

    feat: add SOS signal function

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -6,12 +6,42 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
 }
 
+void blinkSOS() {
+  // Три коротких (точка)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin1, HIGH);
+    delay(150);
+    digitalWrite(ledPin1, LOW);
+    delay(100);
+  }
+  delay(200); // Пауза между буквами
+  // Три длинных (тире)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin1, HIGH);
+    delay(500);
+    digitalWrite(ledPin1, LOW);
+    delay(100);
+  }
+  delay(200);
+  // Три коротких (точка)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPin1, HIGH);
+    delay(150);
+    digitalWrite(ledPin1, LOW);
+    delay(100);
+  }
+  delay(2000); // Пауза перед повтором
+}
+
 void loop() {
-  // Первый светодиод горит, второй гаснет, и наоборот
+  // Старый режим: попеременное мигание
   digitalWrite(ledPin1, HIGH);
   digitalWrite(ledPin2, LOW);
   delay(500);
   digitalWrite(ledPin1, LOW);
   digitalWrite(ledPin2, HIGH);
   delay(500);
+  
+  // Новый режим: вызов SOS
+  blinkSOS();
 }
