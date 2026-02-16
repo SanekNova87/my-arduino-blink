e545ed9b6877484efb749ce3dcc2726ff8074b21
SanekNova87 <rumyantsevaalex.2208@gmail.com>
2026-02-16 19:40:32 +0300

    feat: add second LED on pin 12, alternate blinking

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -6,42 +6,14 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
 }
 
-void blinkSOS() {
-  // Три коротких (точка)
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(ledPin1, HIGH);
-    delay(150);
-    digitalWrite(ledPin1, LOW);
-    delay(100);
-  }
-  delay(200); // Пауза между буквами
-  // Три длинных (тире)
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(ledPin1, HIGH);
-    delay(500);
-    digitalWrite(ledPin1, LOW);
-    delay(100);
-  }
-  delay(200);
-  // Три коротких (точка)
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(ledPin1, HIGH);
-    delay(150);
-    digitalWrite(ledPin1, LOW);
-    delay(100);
-  }
-  delay(2000); // Пауза перед повтором
-}
-
 void loop() {
-  // Старый режим: попеременное мигание
+  // Первый светодиод горит, второй гаснет
   digitalWrite(ledPin1, HIGH);
   digitalWrite(ledPin2, LOW);
   delay(500);
+  
+  // Первый гаснет, второй загорается
   digitalWrite(ledPin1, LOW);
   digitalWrite(ledPin2, HIGH);
   delay(500);
-  
-  // Новый режим: вызов SOS
-  blinkSOS();
 }
